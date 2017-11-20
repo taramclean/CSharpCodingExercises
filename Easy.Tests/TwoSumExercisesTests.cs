67483f907ae584f02d629c60df31d932a5fdb612
Tara McLean <tara.mclean@gmail.com>
2017-11-20 14:36:20 +0000

    Added two sum exercise

---
+++ Easy.Tests/TwoSumExercisesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Easy.Tests
+{
+    public class TwoSumExercisesTests
+    {
+        [Fact]
+        public void TwoSum_ArrayContainingTarget_ReturnsExpectedResult()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var twoSumExercises = new TwoSumExercises();
+
+            var expected = new int[] { 0, 1 };
+            var result = twoSumExercises.TwoSum(nums, target);
+            
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TwoSum_TargetNotFound_ThrowsException()
+        {
+            var nums = new int[] { 0, 1, 2, 3 };
+            var target = 9;
+            var twoSumExercises = new TwoSumExercises();
+            
+            Assert.Throws<ArgumentException>(() => twoSumExercises.TwoSum(nums, target));
+        }
+    }
+}
---
+++ Easy/TwoSumExercises.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easy
+{
+    public class TwoSumExercises
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+                map.Add(nums[i], i);
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
